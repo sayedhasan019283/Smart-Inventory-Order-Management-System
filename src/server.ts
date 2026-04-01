a1beb1bcd3ca8cfdca08b1d407a1f712b23a9362
sayedhasan019283 <sayedhasan019283@gmail.com>
2026-04-01 14:56:48 +0600

    fix: change server.ts file for deployment

--- src/server.ts
+++ src/server.ts
@@ -6,29 +6,26 @@ import config from './config';
 import { errorLogger, logger } from './shared/logger';
 import { socketHelper } from './app/socket/socket';
 
-//uncaught exception
 process.on('uncaughtException', error => {
   errorLogger.error('Unhandled Exception Detected', error);
   process.exit(1);
 });
 
 let server: any;
+
 async function main() {
   try {
     mongoose.connect(config.mongoose.url as string);
     logger.info(colors.green('🚀 Database connected successfully'));
-    const port =
-      typeof config.port === 'number' ? config.port : Number(config.port);
-    server = app.listen(port, config.backendIp as string, () => {
-      logger.info(
-        colors.yellow(
-          `♻️  Application listening on port http://${config.backendIp}:${port}/test`
-        )
-      );
     
-
+    const port = typeof config.port === 'number' ? config.port : Number(config.port);
+    
+    server = app.listen(port, '0.0.0.0', () => {
+      logger.info(
+        colors.yellow(`♻️  Application listening on port http://0.0.0.0:${port}/test`)
+      );
     });
-    //socket
+
     const io = new Server(server, {
       pingTimeout: 60000,
       cors: {
@@ -38,11 +35,11 @@ async function main() {
     socketHelper.socket(io);
     // @ts-ignore
     global.io = io;
+
   } catch (error) {
     errorLogger.error(colors.red('🤢 Failed to connect Database'));
   }
 
-  //handle unhandledRejection
   process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
@@ -57,7 +54,6 @@ async function main() {
 
 main();
 
-//SIGTERM
 process.on('SIGTERM', () => {
   logger.info('SIGTERM IS RECEIVE');
   if (server) {
