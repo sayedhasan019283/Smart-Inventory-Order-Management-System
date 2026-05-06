6d39994c0a2b72d410ede80831e1cf10e9cc0139
sayedhasan019283 <sayedhasan019283@gmail.com>
2026-05-06 11:56:46 +0600

    add: Adding otel basic setup in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,28 @@
+/* instrumentation.mjs */
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base'
+import { resourceFromAttributes } from '@opentelemetry/resources'
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
+
+const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'IOMS-service',
+  }),
+
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-dns': { enabled: false },
+      '@opentelemetry/instrumentation-net': { enabled: false },
+      '@opentelemetry/instrumentation-mongodb': { enabled: true },
+    }),
+  ],
+
+  spanProcessor: new SimpleSpanProcessor(new ConsoleSpanExporter()),
+})
+
+sdk.start()
+
 import colors from 'colors';
 import mongoose from 'mongoose';
 import { Server } from 'socket.io';
@@ -5,6 +30,8 @@ import app from './app';
 import config from './config';
 import { errorLogger, logger } from './shared/logger';
 import { socketHelper } from './app/socket/socket';
+
+
 
 process.on('uncaughtException', error => {
   errorLogger.error('Unhandled Exception Detected', error);
